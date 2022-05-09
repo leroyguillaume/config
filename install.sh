ab02257c8af76f4bc9455b68e9998af65aa5b84b
Guillaume Leroy <pro.guillaume.leroy@gmail.com>
2022-05-09 16:44:39 +0200

    install: improve lisibility

--- install.sh
+++ install.sh
@@ -1,16 +1,27 @@
 #!/bin/bash
 
-set -e
+function run {
+    ${@} >> ${log_file} 2>&1
+    exit_status=${?}
+    if [ ${exit_status} -ne 0 ]; then
+        echo "${@} exited with status ${exit_status}. See ${log_file} for details."
+        exit 2
+    fi
+}
 
 if [ ${USER} != "root" ]; then
     >&2 echo "This script must be run as root"
     exit 1
 fi
 
+log_file=/tmp/install-$(date '+%H%M%S').log
+
 home=$(eval echo ~${SUDO_USER})
 shell=$(cat /etc/passwd | grep ${SUDO_USER} | awk -F : '{print $7}')
 script_dirpath=$(dirname $(readlink -f ${0}))
 files_dirpath=${script_dirpath}/files
+
+packages=(curl direnv htop vim zsh)
 
 tf_docs_version=0.16.0
 
@@ -49,29 +60,42 @@ for config_filename in $(ls ${files_dirpath}); do
 done
 
 # tools
-apt update
-apt install -y curl direnv vim zsh
+echo -n "Updating apt cache... "
+run apt update
+echo "✓"
+echo -n "Installing ${packages[@]}... "
+run apt install -y ${packages}
+echo "✓"
 
 # oh my zsh
 if [ ${shell} != "/usr/bin/zsh" ]; then
     chsh -s /usr/bin/zsh ${SUDO_USER}
+    echo "Shell changed to zsh"
 fi
 if [ ! -d ${home}/.oh-my-zsh ]; then
-    git clone ${home}/.oh-my-zsh
+    echo -n "Cloning oh-my-zsh... "
+    run git clone ${home}/.oh-my-zsh
+    echo "✓"
 fi
 
 # openvpn - update-systemd-resolved
 if [[ -d /etc/openvpn && $(sudo systemctl is-active systemd-resolved) == "active" ]]; then
+    echo -n "Installing update-systemd-resolved... "
     mkdir -p /etc/openvpn/sh
-    curl https://raw.githubusercontent.com/jonathanio/update-systemd-resolved/master/update-systemd-resolved > /etc/openvpn/sh/update-systemd-resolved
+    curl https://raw.githubusercontent.com/jonathanio/update-systemd-resolved/master/update-systemd-resolved > /etc/openvpn/sh/update-systemd-resolved 2>> ${log_file}
     chmod +x /etc/openvpn/sh/update-systemd-resolved
+    echo "✓"
 fi
 
 # tfswitch
-curl -fL https://raw.githubusercontent.com/warrensbox/terraform-switcher/release/install.sh | bash
+echo -n "Installing terraform-switcher... "
+curl -fL https://raw.githubusercontent.com/warrensbox/terraform-switcher/release/install.sh 2>> ${log_file} | bash >> ${log_file} 2>&1
+echo "✓"
 
 # terraform-docs
-curl -fLo /tmp/terraform-docs.tar.gz https://github.com/terraform-docs/terraform-docs/releases/download/v${tf_docs_version}/terraform-docs-v${tf_docs_version}-$(uname)-amd64.tar.gz
+echo -n "Installing terraform-docs v${tf_docs_version}... "
+curl -fLo /tmp/terraform-docs.tar.gz https://github.com/terraform-docs/terraform-docs/releases/download/v${tf_docs_version}/terraform-docs-v${tf_docs_version}-$(uname)-amd64.tar.gz 2>> ${log_file}
 tar xf /tmp/terraform-docs.tar.gz -C /tmp
 chmod +x /tmp/terraform-docs
 mv /tmp/terraform-docs /usr/local/bin
+echo "✓"

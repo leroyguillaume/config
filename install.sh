ef38fd28bf2ae8f7ba06192b0f6b6e476526aaea
Guillaume Leroy <pro.guillaume.leroy@gmail.com>
2022-05-17 12:52:17 +0200

    install: refactor + rustup

--- install.sh
+++ install.sh
@@ -9,11 +9,6 @@ function run {
     fi
 }
 
-if [ ${USER} != "root" ]; then
-    >&2 echo "This script must be run as root"
-    exit 1
-fi
-
 log_file=/tmp/install-$(date '+%H%M%S').log
 
 home=$(eval echo ~${SUDO_USER})
@@ -24,6 +19,18 @@ files_dirpath=${script_dirpath}/files
 packages=(curl direnv htop vim xclip zsh)
 
 tf_docs_version=0.16.0
+
+if [ ${USER} != "root" ]; then
+    >&2 echo "This script must be run as root"
+    exit 1
+fi
+
+while getopts "f" opt; do
+    case ${opt} in
+        f) force=true
+           ;;
+    esac
+done
 
 # config files
 for config_filename in $(ls ${files_dirpath}); do
@@ -79,29 +86,54 @@ if [ ! -d ${home}/.oh-my-zsh ]; then
 fi
 
 # openvpn - update-systemd-resolved
-if [[ -d /etc/openvpn && $(sudo systemctl is-active systemd-resolved) == "active" ]]; then
+if [[ -f /etc/openvpn/sh/update-systemd-resolved && -z "${force}" ]]; then
+    echo "Skipping update-systemd-resolved installation because it is already installed"
+elif [[ -d /etc/openvpn && $(sudo systemctl is-active systemd-resolved) == "active" ]]; then
     echo -n "Installing update-systemd-resolved... "
-    mkdir -p /etc/openvpn/sh
-    curl https://raw.githubusercontent.com/jonathanio/update-systemd-resolved/master/update-systemd-resolved > /etc/openvpn/sh/update-systemd-resolved 2>> ${log_file}
-    chmod +x /etc/openvpn/sh/update-systemd-resolved
+    run mkdir -p /etc/openvpn/sh
+    run curl -Lfo /etc/openvpn/sh/update-systemd-resolved https://raw.githubusercontent.com/jonathanio/update-systemd-resolved/master/update-systemd-resolved
+    run chmod +x /etc/openvpn/sh/update-systemd-resolved
     echo "✓"
 fi
 
 # tfswitch
-echo -n "Installing terraform-switcher... "
-curl -fL https://raw.githubusercontent.com/warrensbox/terraform-switcher/release/install.sh 2>> ${log_file} | bash >> ${log_file} 2>&1
-echo "✓"
+if command -v tfswitch > /dev/null 2>&1 && [ -z "${force}" ]; then
+    echo "Skipping tfswitch installation because it is already installed"
+else
+    echo -n "Installing terraform-switcher... "
+    run curl -Lfo /tmp/install-tfswitch.sh https://raw.githubusercontent.com/warrensbox/terraform-switcher/release/install.sh
+    run bash /tmp/install-tfswitch.sh
+    echo "✓"
+fi
 
 # terraform-docs
-echo -n "Installing terraform-docs v${tf_docs_version}... "
-curl -fLo /tmp/terraform-docs.tar.gz https://github.com/terraform-docs/terraform-docs/releases/download/v${tf_docs_version}/terraform-docs-v${tf_docs_version}-$(uname)-amd64.tar.gz 2>> ${log_file}
-tar xf /tmp/terraform-docs.tar.gz -C /tmp
-chmod +x /tmp/terraform-docs
-mv /tmp/terraform-docs /usr/local/bin
-echo "✓"
+if command -v terraform-docs > /dev/null 2>&1 && [ -z "${force}" ]; then
+    echo "Skipping terraform-docs installation because it is already installed"
+else
+    echo -n "Installing terraform-docs v${tf_docs_version}... "
+    run curl -Lfo /tmp/terraform-docs.tar.gz https://github.com/terraform-docs/terraform-docs/releases/download/v${tf_docs_version}/terraform-docs-v${tf_docs_version}-$(uname)-amd64.tar.gz
+    run tar xf /tmp/terraform-docs.tar.gz -C /tmp
+    run chmod +x /tmp/terraform-docs
+    run mv /tmp/terraform-docs /usr/local/bin
+    echo "✓"
+fi
 
 # ledger live
-echo -n "Installing Ledger Live... "
-curl -fLo /usr/local/bin/ledger-live https://download-live.ledger.com/releases/latest/download/linux 2>> ${log_file}
-chmod +x /usr/local/bin/ledger-live
-echo "✓"
+if command -v ledger-live > /dev/null 2>&1 && [ -z "${force}" ]; then
+    echo "Skipping ledger-live installation because it is already installed"
+else
+    echo -n "Installing Ledger Live... "
+    run curl -fLo /usr/local/bin/ledger-live https://download-live.ledger.com/releases/latest/download/linux
+    run chmod +x /usr/local/bin/ledger-live
+    echo "✓"
+fi
+
+# rustup
+if command -v rustup > /dev/null 2>&1 && [ -z "${force}" ]; then
+    echo "Skipping rustup installation because it is already installed"
+else
+    echo -n "Installing rustup... "
+    run curl -Lfo /tmp/install-rustup.sh https://sh.rustup.rs
+    run bash /tmp/install-rustup.sh -y
+    echo "✓"
+fi
